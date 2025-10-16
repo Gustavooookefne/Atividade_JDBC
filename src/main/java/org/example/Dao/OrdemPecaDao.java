c14a5e5775891c6c0aebf176abbdae486cfbbf29
Gustavooookefne <gustavooookefne14@gmail.com>
2025-10-16 19:49:09 -0300

    feat: Fazendo o ordemPecaDao

--- src/main/java/org/example/Dao/OrdemPecaDao.java
+++ src/main/java/org/example/Dao/OrdemPecaDao.java
@@ -1,4 +1,28 @@
 package org.example.Dao;
 
+import org.example.Model.OrdemPeca;
+import org.example.infra.Conexao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrdemPecaDao {
-}
+    public static void gerarOrdem(OrdemPeca ordemPeca){
+        String slq = """
+                INSERT INTO OrdemPeca
+                (idOrdem , idPeca ,quantidade) VALUES (?,?,?)
+                """;
+        List<OrdemPeca> ordem = new ArrayList<>();
+        try (Connection conn = Conexao.getConnection();
+             PreparedStatement stmt  = conn.prepareStatement(slq)){
+
+                stmt.setString(1,ordemPeca);
+
+
+    } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
