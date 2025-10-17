8e3406b29b639405073fbdc6b0c335ece542f9c3
Gustavooookefne <gustavooookefne14@gmail.com>
2025-10-16 21:44:20 -0300

    feat: Fazendo o ordemManutencaoService

--- src/main/java/org/example/Service/OrdemManutencaoService.java
+++ src/main/java/org/example/Service/OrdemManutencaoService.java
@@ -1,4 +1,15 @@
 package org.example.Service;
 
+import org.example.Dao.OrdemManutencaoDao;
+import org.example.Model.OrdemManutencao;
+
 public class OrdemManutencaoService {
+    private final OrdemManutencaoDao ordemManutencaoDao = new OrdemManutencaoDao();
+
+    public void OrdemManutencao (OrdemManutencao ordemManutencao){
+        try{
+
+        }
+    }
+
 }
