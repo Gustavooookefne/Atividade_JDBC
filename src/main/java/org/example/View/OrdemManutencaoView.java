5923a0385eb46a34dc7465aeaece0acf76bd1432
Gustavooookefne <gustavooookefne14@gmail.com>
2025-10-17 21:35:24 -0300

    feat: Fazendo alterações na View do OrdemManutencao

--- src/main/java/org/example/View/OrdemManutencaoView.java
+++ src/main/java/org/example/View/OrdemManutencaoView.java
@@ -24,31 +24,8 @@ public class OrdemManutencaoView {
         this.pecaDao = pecaDao;
     }
 
-    public void CriarOrdemManutencao(){
+    public void CriarOrdemManutencao() {
         System.out.println("----------- Criar Ordem Manuteção -----------");
 
-        int idMaquina =
-        if (idMaquina == -1) return;
-
-        int idTecnico = selecionarTecnico();
-        if (idTecnico == -1) return;
-
-        try {
-            // 4. Data de solicitação = data atual
-            // 5. Status inicial = PENDENTE
-            OrdemManutencao novaOrdem = new OrdemManutencao(idMaquina, idTecnico);
-
-            // 6. Inserção no banco e 7. Atualizar status da máquina (Service)
-            int idOrdem = ordemService.criarOrdem(novaOrdem);
-
-            // 8. Confirmação
-            System.out.println("\n[SUCESSO] Ordem de Manutenção ID " + idOrdem + " criada e máquina atualizada!");
-
-        } catch (RegraNegocioException e) {
-            System.err.println("[ERRO DE REGRA] " + e.getMessage());
-        } catch (SQLException e) {
-            System.err.println("[ERRO DE BANCO] Falha na criação da Ordem: " + e.getMessage());
-        }
-    }
     }
 }
