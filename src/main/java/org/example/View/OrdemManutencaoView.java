d094607426bd3b60e219d100be04bcda569b4988
Gustavooookefne <gustavooookefne14@gmail.com>
2025-10-17 21:30:49 -0300

    feat: Começando a fazer a View do OrdemManutencao

--- src/main/java/org/example/View/OrdemManutencaoView.java
+++ src/main/java/org/example/View/OrdemManutencaoView.java
@@ -1,4 +1,30 @@
 package org.example.View;
 
+import org.example.Dao.MaquinaDAO;
+import org.example.Dao.PecaDao;
+import org.example.Dao.TecnicoDao;
+import org.example.Model.OrdemManutencao;
+import org.example.Service.OrdemManutencaoService;
+
+import java.util.Scanner;
+
 public class OrdemManutencaoView {
+
+    private final Scanner sc;
+    private OrdemManutencaoService ordemManutencaoService;
+
+    private final MaquinaDAO maquinaDAO;
+    private final TecnicoDao tecnicoDao;
+    private final PecaDao pecaDao;
+
+    public OrdemManutencaoView(Scanner sc, MaquinaDAO maquinaDAO, TecnicoDao tecnicoDao, PecaDao pecaDao) {
+        this.sc = sc;
+        this.maquinaDAO = maquinaDAO;
+        this.tecnicoDao = tecnicoDao;
+        this.pecaDao = pecaDao;
+    }
+
+    public void CriarOrdemManutencao(){
+        System.out.println("----------- Criar Ordem Manuteção -----------");
+    }
 }
